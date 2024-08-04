6b8afee7a810f9f54602924ae24ea1910ab7b444
Hendro <hendrowunga073@gmail.com>
2024-08-04 20:11:20 +0700

    add code repository

--- src/main/java/com/hendro/demo/repository/UserRepository.java
+++ src/main/java/com/hendro/demo/repository/UserRepository.java
@@ -2,17 +2,17 @@ package com.hendro.demo.repository;
 
 import java.util.Optional;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.hendro.demo.model.User;
 
 @Repository
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String Email);
 
-    Optional<User> finByVertificationCode(String vertificationCode);
+    Optional<User> findByVerificationCode(String verificationCode);
 
 }
